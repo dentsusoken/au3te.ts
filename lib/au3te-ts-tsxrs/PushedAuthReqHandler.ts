2a7b8b8b6784106677a104d63aad7d49195759d9
kenta-ando <xando.kenta@contract.dentsusoken.com>
2024-04-11 15:27:09 +0900

    [implement]Params

--- lib/au3te-ts-tsxrs/PushedAuthReqHandler.ts
+++ lib/au3te-ts-tsxrs/PushedAuthReqHandler.ts
@@ -8,131 +8,173 @@ import { BaseHandler } from './BaseHandler';
 /**
  * Handler for pushed authorization request endpoint requests.
  */
-export class PushedAuthReqHandler extends BaseHandler {
+export default class PushedAuthReqHandler extends BaseHandler {
     /**
      * Parameters passed to the `handle` method.
      */
-    export interface Params {
-        parameters: MultivaluedMap<string, string>;
-        authorization: string;
-        clientCertificatePath: string[];
-        dpop: string;
-        htm: string;
-        htu: string;
-    }
+    //     class Params {
+    //     private parameters?: { [key: string]: string };
+    //     private authorization?: string;
+    //     private clientCertificatePath?: string[];
+    //     private dpop?: string;
+    //     private htm?: string;
+    //     private htu?: string;
+
+    //     public setparameters(): { [key: string]: string } | undefined {
+    //         return this.parameters;
+    //     }
+    //     public setAuthorization(): string | undefined {
+    //         return this.authorization;
+    //     }
+    //     public setclientCertificatePath(): string[] | undefined {
+    //         return this.clientCertificatePath;
+    //     }
+    //     public setdpop(): string | undefined {
+    //         return this.dpop;
+    //     }
+    //     public sethtm(): string | undefined {
+    //         return this.htm;
+    //     }
+    // }
 
     /**
      * Constructor with an implementation of `AuthleteApi` interface.
      */
-    constructor(api: AuthleteApi) {
-        super(api);
-    }
+    // constructor(api: AuthleteApi) {
+    //     super(api);
+    // }
 
     /**
      * Handle a pushed authorization request.
      */
-    handle(parameters: MultivaluedMap<string, string>, authorization: string, clientCertificatePath: string[]): Response {
-        const params: Params = {
-            parameters,
-            authorization,
-            clientCertificatePath,
-            dpop: '',
-            htm: '',
-            htu: ''
-        };
+    // handle(parameters: MultivaluedMap<string, string>, authorization: string, clientCertificatePath: string[]): Response {
+    //     const params: Params = {
+    //         parameters,
+    //         authorization,
+    //         clientCertificatePath,
+    //         dpop: '',
+    //         htm: '',
+    //         htu: ''
+    //     };
 
-        return this.handle(params);
-    }
+    //     return this.handle(params);
+    // }
 
     /**
      * Handle a PAR request.
      */
-    handle(params: Params): Response {
-        const credentials = BasicCredentials.parse(params.authorization);
-        const clientId = credentials?.getUserId() ?? null;
-        const clientSecret = credentials?.getPassword() ?? null;
+    // handle(params: Params): Response {
+    //     const credentials = BasicCredentials.parse(params.authorization);
+    //     const clientId = credentials?.getUserId() ?? null;
+    //     const clientSecret = credentials?.getPassword() ?? null;
 
-        try {
-            return this.process(
-                params.parameters,
-                clientId,
-                clientSecret,
-                params.clientCertificatePath,
-                params.dpop,
-                params.htm,
-                params.htu
-            );
-        } catch (e) {
-            if (e instanceof WebApplicationException) {
-                throw e;
-            } else {
-                throw new Error('Unexpected error in PushedAuthReqHandler');
-            }
-        }
-    }
+    //     try {
+    //         return this.process(
+    //             params.parameters,
+    //             clientId,
+    //             clientSecret,
+    //             params.clientCertificatePath,
+    //             params.dpop,
+    //             params.htm,
+    //             params.htu
+    //         );
+    //     } catch (e) {
+    //         if (e instanceof WebApplicationException) {
+    //             throw e;
+    //         } else {
+    //             throw new Error('Unexpected error in PushedAuthReqHandler');
+    //         }
+    //     }
+    // }
 
     /**
      * Process the parameters of the pushed authorization request.
      */
-    private process(
-        parameters: MultivaluedMap<string, string>,
-        clientId: string | null,
-        clientSecret: string | null,
-        clientCertificatePath: string[] | null,
-        dpop: string,
-        htm: string,
-        htu: string
-    ): Response {
-        let clientCertificate: string | null = null;
-        if (clientCertificatePath && clientCertificatePath.length > 0) {
-            clientCertificate = clientCertificatePath[0];
+    // private process(
+    //     parameters: MultivaluedMap<string, string>,
+    //     clientId: string | null,
+    //     clientSecret: string | null,
+    //     clientCertificatePath: string[] | null,
+    //     dpop: string,
+    //     htm: string,
+    //     htu: string
+    // ): Response {
+    //     let clientCertificate: string | null = null;
+    //     if (clientCertificatePath && clientCertificatePath.length > 0) {
+    //         clientCertificate = clientCertificatePath[0];
 
-            if (clientCertificatePath.length > 1) {
-                clientCertificatePath = clientCertificatePath.slice(1);
-            }
-        }
+    //         if (clientCertificatePath.length > 1) {
+    //             clientCertificatePath = clientCertificatePath.slice(1);
+    //         }
+    //     }
 
-        const response: PushedAuthReqResponse = this.getApiCaller().callPushedAuthReq(
-            parameters,
-            clientId,
-            clientSecret,
-            clientCertificate,
-            clientCertificatePath,
-            dpop,
-            htm,
-            htu
-        );
+    //     const response: PushedAuthReqResponse = this.getApiCaller().callPushedAuthReq(
+    //         parameters,
+    //         clientId,
+    //         clientSecret,
+    //         clientCertificate,
+    //         clientCertificatePath,
+    //         dpop,
+    //         htm,
+    //         htu
+    //     );
 
-        const action: Action = response.getAction();
-        const content: string = response.getResponseContent();
-        const headers: Map<string, Object> = this.prepareHeaders(response);
+    //         const action: Action = response.getAction();
+    //         const content: string = response.getResponseContent();
+    //         const headers: Map<string, Object> = this.prepareHeaders(response);
 
-        switch (action) {
-            case Action.BAD_REQUEST:
-                return ResponseUtil.badRequest(content, headers);
-            case Action.CREATED:
-                return ResponseUtil.created(content, headers);
-            case Action.FORBIDDEN:
-                return ResponseUtil.forbidden(content, headers);
-            case Action.INTERNAL_SERVER_ERROR:
-                return ResponseUtil.internalServerError(content, headers);
-            case Action.PAYLOAD_TOO_LARGE:
-                return ResponseUtil.tooLarge(content, headers);
-            case Action.UNAUTHORIZED:
-                return ResponseUtil.unauthorized(content, null, headers);
-            default:
-                throw this.getApiCaller().unknownAction('/api/pushed_auth_req', action);
-        }
+    //         switch (action) {
+    //             case Action.BAD_REQUEST:
+    //                 return ResponseUtil.badRequest(content, headers);
+    //             case Action.CREATED:
+    //                 return ResponseUtil.created(content, headers);
+    //             case Action.FORBIDDEN:
+    //                 return ResponseUtil.forbidden(content, headers);
+    //             case Action.INTERNAL_SERVER_ERROR:
+    //                 return ResponseUtil.internalServerError(content, headers);
+    //             case Action.PAYLOAD_TOO_LARGE:
+    //                 return ResponseUtil.tooLarge(content, headers);
+    //             case Action.UNAUTHORIZED:
+    //                 return ResponseUtil.unauthorized(content, null, headers);
+    //             default:
+    //                 throw this.getApiCaller().unknownAction('/api/pushed_auth_req', action);
+    //         }
+    //     }
+
+    //     private prepareHeaders(response: PushedAuthReqResponse): Map<string, Object> {
+    //         const headers: Map<string, Object> = new LinkedHashMap<string, Object>();
+
+    //         const dpopNonce: string | null = response.getDpopNonce();
+    //         if (dpopNonce) {
+    //             headers.put('DPoP-Nonce', dpopNonce);
+    //         }
+
+    //         return headers;
+    //     }
+}
+
+// namespace PushedAuthReqHandler {
+export class Params {
+    private parameters?: { [key: string]: string };
+    private authorization?: string;
+    private clientCertificatePath?: string[];
+    private dpop?: string;
+    private htm?: string;
+    private htu?: string;
+
+    public setparameters(): { [key: string]: string } | undefined {
+        return this.parameters;
     }
-
-    private prepareHeaders(response: PushedAuthReqResponse): Map<string, Object> {
-        const headers: Map<string, Object> = new LinkedHashMap<string, Object>();
-
-        const dpopNonce: string | null = response.getDpopNonce();
-        if (dpopNonce) {
-            headers.put('DPoP-Nonce', dpopNonce);
-        }
-
-        return headers;
+    public setAuthorization(): string | undefined {
+        return this.authorization;
+    }
+    public setclientCertificatePath(): string[] | undefined {
+        return this.clientCertificatePath;
+    }
+    public setdpop(): string | undefined {
+        return this.dpop;
+    }
+    public sethtm(): string | undefined {
+        return this.htm;
     }
 }
