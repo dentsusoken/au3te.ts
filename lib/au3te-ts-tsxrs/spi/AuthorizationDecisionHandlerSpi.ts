b4d08e91488041ec45ca61dc3551a920c269cd7d
kenta-ando <xando.kenta@contract.dentsusoken.com>
2024-05-02 14:46:49 +0900

    WIP Implement Authorization endpoint

--- lib/au3te-ts-tsxrs/spi/AuthorizationDecisionHandlerSpi.ts
+++ lib/au3te-ts-tsxrs/spi/AuthorizationDecisionHandlerSpi.ts
@@ -1,2 +1,12 @@
-// TODO Authorization Endpoint
-export interface AuthorizationDecisionHandlerSpi {}
+// TODO à¿ì°é¿ëïçœÇ›
+export interface AuthorizationDecisionHandlerSpi {
+    isClientAuthorized(): boolean;
+    getUserSubject(): string;
+    getSub(): string;
+    getUserAuthenticatedAt(): number;
+    getAcr(): string;
+    getUserClaim(claimName: string, languageTag?: string): string;
+    getVerifiedClaims(subject?: string, verifiedClaimsRequest?: unknown): unknown;
+    getProperties(): Property[];
+    getScopes(): string[];
+}
