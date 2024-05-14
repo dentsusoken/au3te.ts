850fcf2e12c685d43c502699c6aeb9f9b8c07271
kenta-ando <xando.kenta@contract.dentsusoken.com>
2024-05-14 17:35:25 +0900

    WIP Implement Token Endpoint

--- lib/au3te-ts-common/dto/TokenResponse.spec.ts
+++ lib/au3te-ts-common/dto/TokenResponse.spec.ts
@@ -1,1 +1,17 @@
-// TODO Token Endpoint
+// TODO 安藤実装済み
+import { describe, expect, it } from 'vitest';
+import { TokenResponse } from './TokenResponse';
+
+describe('TokenRequest', () => {
+    it('getResponseContent', () => {
+        const response = new TokenResponse();
+        response.setResponseContent('ResContent');
+        expect(response.getResponseContent()).toEqual('ResContent')
+    })
+
+    it('getDpopNonce', () => {
+        const response = new TokenResponse();
+        response.setDpopNonce('DpNonce');
+        expect(response.getDpopNonce()).toEqual('DpNonce')
+    })
+})
